eb40de21f00df72fad0043b8acb119f84d0022ac
Tomas19840823 <tomas.usinskas@gmail.com>
2017-04-06 16:58:34 +0300

    Add files via upload

---
+++ R/down.R
@@ -0,0 +1,17 @@
+down <- function(x)  {
+  
+  m <- matrix(nrow = 1, ncol = 6)
+  
+  for (i in 1:length(x)) {
+    
+    matr <- matrix(x[[i]][[1]], ncol = 4)
+    eile <- match(unlist(x[[i]][[2]][3]),as.matrix(x[[i]][[1]])[,1])
+    dist <- rep(as.numeric(unlist(x[[i]][[2]][2])),length(eile))
+    auto <- rep(x[[i]][[3]],length(eile))
+    m1 <- cbind(matrix(matr[eile,],ncol = 4),as.matrix(auto),as.matrix(dist))
+    m <- rbind(m,m1)
+  }
+  m <- m[-1,]
+  
+  return(m)
+}

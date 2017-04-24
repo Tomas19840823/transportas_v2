32bb7c644ed88681a9554829c99e00c031ff5387
Tomas19840823 <tomas.usinskas@gmail.com>
2017-04-24 15:31:17 +0300

    Update autolentele.R

--- R/autolentele.R
+++ R/autolentele.R
@@ -1,8 +1,8 @@
 autolentele <- function(){
   mat <- matrix(nrow = 8, ncol = 3)
   mat[,1] <- c("ABC123", "DEF456", "GHI789","JKL123","MNO456","PRS789","TUV123","ZZZ456")
-  mat[,2] <- c(50, 50, 50, 50, 50, 50, 50, 50)
-  mat[,3] <- c(50, 50, 50, 50, 50, 50, 50, 50)
+  mat[,2] <- c(100, 100, 100, 100, 100, 100, 100, 100)
+  mat[,3] <- c(100, 100, 100, 100, 100, 100, 100, 100)
  
   
   colnames(mat) <- c("auto nr", "svoris min", "svoris max")
